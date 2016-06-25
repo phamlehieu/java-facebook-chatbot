d442d891c7c0987d55285cb810f7a5f1599d0cf3
phamlehieu <phamlehieu@gmail.com>
2016-06-26 01:21:12 +0700

    Add configuration to auto deploy application

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+echo "stopping running application"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop chatbot'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm chatbot'
+
+echo "pulling latest version of the code"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull hieupham/java-fb-chat'
+
+echo "starting the new version"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run --name chatbot --rm -it -p 8080:8080 hieupham/java-fb-chat'
+
+echo "success!"
+
+exit 0
