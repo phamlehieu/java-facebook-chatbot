f01836e0b2ec8012376fdbe9fb5a79c8bb5ba99a
phamlehieu <phamlehieu@gmail.com>
2016-06-25 23:42:53 +0700

    Update eviromentSetup

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -2,7 +2,7 @@ function copyEnvVarsToApplicationProperties {
     APPLICATION_PROPERTIES=$HOME"/java-facebook-chatbot/src/main/resources/application.properties"
     export APPLICATION_PROPERTIES
     echo "Application Properties should exist at $APPLICATION_PROPERTIES"
-    sed 's/^validation.token=.*/validation.token=$VALIDATION_TOKEN/g' $APPLICATION_PROPERTIES
-    sed 's/^app.secret=.*/app.secret=$APP_SECRET/g' $APPLICATION_PROPERTIES
-    sed 's/^page.access.token=.*/age.access.token=$PAGE_ACCESS_TOKEN/g' $APPLICATION_PROPERTIES
+    sed -i 's/^validation.token=.*/validation.token=$VALIDATION_TOKEN/g' $APPLICATION_PROPERTIES
+    sed -i 's/^app.secret=.*/app.secret=$APP_SECRET/g' $APPLICATION_PROPERTIES
+    sed -i 's/^page.access.token=.*/age.access.token=$PAGE_ACCESS_TOKEN/g' $APPLICATION_PROPERTIES
 }
